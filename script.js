5c8982087dd270c78eb2a585dcbc9eee5435f671
ARafay007 <ARafay007>
2021-07-16 06:40:40 -0700

    page completed with design 1

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const id = document.querySelector('.col1');
+const name = document.querySelector('.col2');
+const Class = document.querySelector('.col3');
+const maths = document.querySelector('.Maths');
+const english = document.querySelector('.English');
+const geography = document.querySelector('.Geography');
+const Islamiyat = document.querySelector('.Islamiyat');
+const science = document.querySelector('.Science');
+const urdu = document.querySelector('.Urdu');
+const sindhi = document.querySelector('.Sindhi');
+const dataRow = document.querySelector('.table_container');
+const saveBtn = document.querySelector('.btn-insert');
+
+const addStdData = function(){
+    const 
+};
+
+saveBtn.addEventListener('click', )
